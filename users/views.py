477f1b6f1caea410aef052c1c48b2b58618a7b5c
jijinshan <jijinshan@163.com>
2020-12-07 18:27:22 +0800

    Django基础-请求数据处理 & 添加数据库 & 添加数据到数据表

--- users/views.py
+++ users/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from users.models import User
 
 
 def register(request):
-    return render(request, "register.html")
+    if request.method == "GET":
+        return render(request, "register.html")
+    else:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        User.objects.create(username=username, password=password)
+        return HttpResponse("提交注册请求")
