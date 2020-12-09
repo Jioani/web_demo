14af5ad69160f06b1e58091abc7f4bbec4d4cfec
jijinshan <jijinshan@163.com>
2020-12-09 12:19:22 +0800

    Django基础: 页面重定向 & 登录记住用户名

--- users/views.py
+++ users/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
 from users.models import User
+# Create your views here.
 
 
 def register(request):
@@ -12,4 +11,23 @@ def register(request):
         username = request.POST.get("username")
         password = request.POST.get("password")
         User.objects.create(username=username, password=password)
-        return HttpResponse("提交注册请求")
+        return redirect("/login/")
+
+
+def login(request):
+    if request.method == "GET":
+        username = request.COOKIES.get("username", "")
+        return render(request, "login.html", context={"username": username})
+    else:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        remember = request.POST.get("remember")
+    try:
+        user = User.objects.get(username=username, password=password)
+    except Exception:
+        return JsonResponse({"message": "login failed"})
+    else:
+        response = JsonResponse({"message": "login success"})
+        if remember == "true":
+            response.set_cookie("username", username, max_age=14 * 24 * 3600)
+        return response
